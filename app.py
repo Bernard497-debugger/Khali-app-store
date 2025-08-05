7a2779e1aa14717b94eef3bab03ad5546870efce
Bernard497-debugger <botsile55@gmail.com>
2025-08-05 04:02:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,84 +1,67 @@
-from flask import Flask, request, jsonify, send_from_directory, redirect
+from flask import Flask, request, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
 import os
-import json
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
-DATA_FILE = 'data.json'
+ICON_FOLDER = 'icons'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ICON_FOLDER'] = ICON_FOLDER
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-# Initialize JSON file if not present
-if not os.path.exists(DATA_FILE):
-    with open(DATA_FILE, 'w') as f:
-        json.dump([], f)
+os.makedirs(ICON_FOLDER, exist_ok=True)
 
 @app.route('/')
-def index():
-    return send_from_directory('.', 'index.html')
-
-@app.route('/admin')
-def admin():
-    return send_from_directory('.', 'admin.html')
-
-@app.route('/apps')
-def get_apps():
-    with open(DATA_FILE) as f:
-        return jsonify(json.load(f))
+def home():
+    return "App Store Backend Running!"
 
 @app.route('/upload', methods=['POST'])
-def upload_app():
-    file = request.files['file']
+def upload():
+    apk = request.files['apk']
     icon = request.files['icon']
     name = request.form['name']
-    desc = request.form['description']
+    description = request.form['description']
 
-    file_name = secure_filename(file.filename)
-    icon_name = secure_filename(icon.filename)
+    apk_filename = secure_filename(apk.filename)
+    icon_filename = secure_filename(icon.filename)
 
-    file.save(os.path.join(UPLOAD_FOLDER, file_name))
-    icon.save(os.path.join(UPLOAD_FOLDER, icon_name))
+    apk.save(os.path.join(UPLOAD_FOLDER, apk_filename))
+    icon.save(os.path.join(ICON_FOLDER, icon_filename))
 
-    with open(DATA_FILE) as f:
-        apps = json.load(f)
+    return jsonify({'status': 'success'})
 
-    apps.append({
-        'name': name,
-        'description': desc,
-        'filename': file_name,
-        'icon': icon_name
-    })
-
-    with open(DATA_FILE, 'w') as f:
-        json.dump(apps, f)
-
-    return redirect('/admin')
-
-@app.route('/delete', methods=['POST'])
-def delete_app():
-    index = int(request.form['index'])
-
-    with open(DATA_FILE) as f:
-        apps = json.load(f)
-
-    if 0 <= index < len(apps):
-        # Delete files
-        try:
-            os.remove(os.path.join(UPLOAD_FOLDER, apps[index]['filename']))
-            os.remove(os.path.join(UPLOAD_FOLDER, apps[index]['icon']))
-        except:
-            pass
-        # Remove from list
-        apps.pop(index)
-        with open(DATA_FILE, 'w') as f:
-            json.dump(apps, f)
-
-    return redirect('/')
-
-@app.route('/uploads/<path:filename>')
-def serve_uploads(filename):
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
+@app.route('/icons/<filename>')
+def icon_file(filename):
+    return send_from_directory(ICON_FOLDER, filename)
+
+@app.route('/list')
+def list_files():
+    files = os.listdir(UPLOAD_FOLDER)
+    data = []
+    for file in files:
+        icon_name = file.replace('.apk', '.png')
+        data.append({
+            'name': file.replace('.apk', ''),
+            'file': file,
+            'icon': icon_name,
+            'description': ''
+        })
+    return jsonify(data)
+
+@app.route('/delete', methods=['POST'])
+def delete_file():
+    filename = request.form['filename']
+    try:
+        os.remove(os.path.join(UPLOAD_FOLDER, filename))
+        icon_filename = filename.replace('.apk', '.png')
+        os.remove(os.path.join(ICON_FOLDER, icon_filename))
+        return jsonify({'status': 'deleted'})
+    except:
+        return jsonify({'status': 'error'})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
